6e5cd14f349e26a6b985e71dcc787ba6b85c5f89
qaziok <72215548+qaziok@users.noreply.github.com>
2023-05-25 23:36:46 +0200

    sql server and mysql schema in pl

--- database/init/TERMINAL.sql
+++ database/init/TERMINAL.sql
@@ -1,114 +1,103 @@
-CREATE DATABASE `TERMINAL`;
-
-CREATE TABLE `Projekt` (
-  `ID` ID PRIMARY KEY,
-  `nazwa` string,
-  `is_active` bool
+CREATE TABLE Osoba (
+	ID INT AUTO_INCREMENT PRIMARY KEY,
+	nick VARCHAR(20),
+	Imie VARCHAR(20) NOT NULL,
+	Nazwisko VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE `Osoba` (
-  `ID` ID PRIMARY KEY,
-  `nick` string,
-  `Imie` string,
-  `Nazwisko` string
+CREATE TABLE Przepis (
+	ID INT AUTO_INCREMENT PRIMARY KEY,
+	nazwa VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE `Tagi` (
-  `tag` string PRIMARY KEY,
-  `is_active` bool
+CREATE TABLE Pomiar (
+	ID INT AUTO_INCREMENT PRIMARY KEY,
+	dataPomiaru DATE NOT NULL,
+	komentarz VARCHAR(255),
+	osobaID INT NOT NULL,
+	przepisID INT,
+	FOREIGN KEY (osobaID) REFERENCES Osoba(ID),
+	FOREIGN KEY (przepisID) REFERENCES Przepis(ID)
 );
 
-CREATE TABLE `Etap` (
-  `ID` ID PRIMARY KEY,
-  `komentarz` string,
-  `pomiar` ID
+CREATE TABLE Tag (
+	nazwa VARCHAR(30) PRIMARY KEY,
+	is_active BIT(1) DEFAULT 0
 );
 
-CREATE TABLE `Przepis` (
-  `ID` ID PRIMARY KEY,
-  `nazwa` string
+/* tabela asocjacyjna pomiedzy Pomiarami a Tagami */
+CREATE TABLE TagiPomiaru (
+	pomiarID INT NOT NULL,
+	tag VARCHAR(30) NOT NULL,
+	PRIMARY KEY (pomiarID, tag),
+	FOREIGN KEY (pomiarID) REFERENCES Pomiar(ID),
+	FOREIGN KEY (tag) REFERENCES Tag(nazwa)
 );
 
-CREATE TABLE `Parametr` (
-  `nazwa` string PRIMARY KEY,
-  `is_active` bool,
-  `jednosta` string
+CREATE TABLE Projekt (
+	ID INT AUTO_INCREMENT PRIMARY KEY,
+	nazwa VARCHAR(30) NOT NULL,
+	is_active BIT(1) DEFAULT 0
 );
 
-CREATE TABLE `SlownikWartosci` (
-  `ID` ID,
-  `wartosc` string,
-  `is_active` bool,
-  `parametr` str,
-  PRIMARY KEY (`ID`, `wartosc`)
+/* tabela asocjacyjna pomiedzy Projektami a Pomiarami */
+CREATE TABLE PomiaryProjektu (
+	projektID INT NOT NULL,
+	pomiarID INT NOT NULL,
+	PRIMARY KEY (projektID, pomiarID),
+	FOREIGN KEY (projektID) REFERENCES Projekt(ID),
+	FOREIGN KEY (pomiarID) REFERENCES Pomiar(ID)
 );
 
-CREATE TABLE `EtapPrzepisu` (
-  `ID` ID PRIMARY KEY,
-  `komentarz` string,
-  `przepis` ID
+CREATE TABLE EtapPrzepisu (
+	ID INT AUTO_INCREMENT PRIMARY KEY,
+	przepisID INT NOT NULL,
+	komentarz VARCHAR(255),
+	FOREIGN KEY (przepisID) REFERENCES Przepis(ID)
 );
 
-CREATE TABLE `Pomiar` (
-  `ID` ID PRIMARY KEY,
-  `date` date,
-  `komentarz` string,
-  `osoba` ID,
-  `przepis` ID
+CREATE TABLE Parametr (
+	nazwa VARCHAR(20) PRIMARY KEY,
+	is_active BIT(1) DEFAULT 0,
+	jednostka VARCHAR(10) NOT NULL
 );
 
-ALTER TABLE `Etap` ADD FOREIGN KEY (`pomiar`) REFERENCES `Pomiar` (`ID`);
-
-CREATE TABLE `Etap_SlownikWartosci` (
-  `Etap_ID` ID,
-  `SlownikWartosci_ID` ID,
-  PRIMARY KEY (`Etap_ID`, `SlownikWartosci_ID`)
+CREATE TABLE Etap (
+	ID INT AUTO_INCREMENT PRIMARY KEY,
+	komentarz VARCHAR(255),
+	pomiarID INT NOT NULL,
+	FOREIGN KEY (pomiarID) REFERENCES Pomiar(ID)
 );
 
-ALTER TABLE `Etap_SlownikWartosci` ADD FOREIGN KEY (`Etap_ID`) REFERENCES `Etap` (`ID`);
-
-ALTER TABLE `Etap_SlownikWartosci` ADD FOREIGN KEY (`SlownikWartosci_ID`) REFERENCES `SlownikWartosci` (`ID`);
-
-
-ALTER TABLE `SlownikWartosci` ADD FOREIGN KEY (`parametr`) REFERENCES `Parametr` (`nazwa`);
-
-ALTER TABLE `EtapPrzepisu` ADD FOREIGN KEY (`przepis`) REFERENCES `Przepis` (`ID`);
-
-CREATE TABLE `EtapPrzepisu_SlownikWartosci` (
-  `EtapPrzepisu_ID` ID,
-  `SlownikWartosci_ID` ID,
-  PRIMARY KEY (`EtapPrzepisu_ID`, `SlownikWartosci_ID`)
+CREATE TABLE SlownikWartosci (
+	ID INT AUTO_INCREMENT PRIMARY KEY,
+	wartosc VARCHAR(20) NOT NULL,
+	is_active BIT(1),
+	parametr VARCHAR(20),
+	FOREIGN KEY (parametr) REFERENCES Parametr(nazwa)
 );
 
-ALTER TABLE `EtapPrzepisu_SlownikWartosci` ADD FOREIGN KEY (`EtapPrzepisu_ID`) REFERENCES `EtapPrzepisu` (`ID`);
-
-ALTER TABLE `EtapPrzepisu_SlownikWartosci` ADD FOREIGN KEY (`SlownikWartosci_ID`) REFERENCES `SlownikWartosci` (`ID`);
-
-
-ALTER TABLE `Pomiar` ADD FOREIGN KEY (`osoba`) REFERENCES `Osoba` (`ID`);
-
-ALTER TABLE `Pomiar` ADD FOREIGN KEY (`przepis`) REFERENCES `Przepis` (`ID`);
-
-CREATE TABLE `Pomiar_Projekt` (
-  `Pomiar_ID` ID,
-  `Projekt_ID` ID,
-  PRIMARY KEY (`Pomiar_ID`, `Projekt_ID`)
+/* tabela asocjacyjna pomiedzy Etapami a SlownikiemWartosci */
+CREATE TABLE EtapySlownik(
+	etapID INT NOT NULL,
+	slownikID INT NOT NULL,
+	PRIMARY KEY (etapID, slownikID),
+	FOREIGN KEY (etapID) REFERENCES Etap(ID),
+	FOREIGN KEY (slownikID) REFERENCES SlownikWartosci(ID)
 );
 
-ALTER TABLE `Pomiar_Projekt` ADD FOREIGN KEY (`Pomiar_ID`) REFERENCES `Pomiar` (`ID`);
-
-ALTER TABLE `Pomiar_Projekt` ADD FOREIGN KEY (`Projekt_ID`) REFERENCES `Projekt` (`ID`);
-
-
-CREATE TABLE `Pomiar_Tagi` (
-  `Pomiar_ID` ID,
-  `Tagi_tag` string,
-  PRIMARY KEY (`Pomiar_ID`, `Tagi_tag`)
+/* tabela asocjacyjna pomiedzy EtapamiPrzepisu a SlownikiemWartosci */
+CREATE TABLE EtapyPrzepisuSlownik (
+	etapPrzepisuID INT NOT NULL,
+	slownikID INT NOT NULL,
+	PRIMARY KEY (etapPrzepisuID, slownikID),
+	FOREIGN KEY (etapPrzepisuID) REFERENCES EtapPrzepisu(ID),
+	FOREIGN KEY (slownikID) REFERENCES SlownikWartosci(ID)
 );
 
-ALTER TABLE `Pomiar_Tagi` ADD FOREIGN KEY (`Pomiar_ID`) REFERENCES `Pomiar` (`ID`);
-
-ALTER TABLE `Pomiar_Tagi` ADD FOREIGN KEY (`Tagi_tag`) REFERENCES `Tagi` (`tag`);
-
-
-ALTER TABLE `Pomiar` ADD FOREIGN KEY (`date`) REFERENCES `Pomiar` (`osoba`);
+/*
+TODO:
+1. dodac defaultowe wartosci, np do 'is_active' #to check
+2. dodac NOT NULL tam gdzie trzeba #to check
+3. relacja pomiedzy dat� a osob� w tabeli Pomiar?
+*/
