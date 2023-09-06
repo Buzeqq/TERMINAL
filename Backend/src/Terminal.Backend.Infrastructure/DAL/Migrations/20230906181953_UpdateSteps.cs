a763fa6b2d05633d1e570dc271ba05aa00ef883f
buzeqq <miloszchoj@gmail.com>
2023-09-06 20:33:29 +0200

    migrations

---
+++ Backend/src/Terminal.Backend.Infrastructure/DAL/Migrations/20230906181953_UpdateSteps.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Terminal.Backend.Infrastructure.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSteps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Steps_Recipes_RecipeId",
+                table: "Steps");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "RecipeId",
+                table: "Steps",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Steps_Recipes_RecipeId",
+                table: "Steps",
+                column: "RecipeId",
+                principalTable: "Recipes",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Steps_Recipes_RecipeId",
+                table: "Steps");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "RecipeId",
+                table: "Steps",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Steps_Recipes_RecipeId",
+                table: "Steps",
+                column: "RecipeId",
+                principalTable: "Recipes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
