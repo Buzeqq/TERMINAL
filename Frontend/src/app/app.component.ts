e8165f5a14bab82b7aaeba523456e37a86376f18
qaziok <72215548+qaziok@users.noreply.github.com>
2023-12-02 19:14:04 +0100

    sidenav works

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { map, Observable } from "rxjs";
 import { BreakpointObserver } from "@angular/cdk/layout";
 import { AddProjectDialogComponent } from "./core/components/dialogs/add-project/add-project-dialog.component";
@@ -6,6 +6,7 @@ import { MatDialog } from "@angular/material/dialog";
 import { Router } from "@angular/router";
 import {AuthService} from "./core/services/auth/auth.service";
 import {NotificationService} from "./core/services/notification/notification.service";
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -13,12 +14,15 @@ import {NotificationService} from "./core/services/notification/notification.ser
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  isExpanded: boolean = false;
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  isUserLoggedOut$ = this.authService.isLoggedOut();
+  
+  isMobile$: Observable<boolean> = this.breakpointObserver.observe('(max-width: 768px)').pipe(map(result => result.matches));
+  isMobile: boolean = false;
 
-  menuOpened$: Observable<boolean> = this.breakpointObserver.observe('(max-width: 768px)')
-    .pipe(map(result => !result.matches));
-
-  isLoggedOut$ = this.authService.isLoggedOut();
+  // honestly, I don't know why this works, but it does and I'm not going to question it
+  isExpanded$: Observable<boolean> = this.isMobile$.pipe(map(result => result || !this.isMobile));
+  isExpanded: boolean = false // toggleable
 
   constructor(
     private readonly breakpointObserver: BreakpointObserver,
@@ -36,5 +40,39 @@ export class AppComponent implements OnInit {
     this.authService.sessionWarningTimer$
       .subscribe(_ => this.snackbar.notifySessionExpiration(this.authService.alertBefore)
         .subscribe(_ => this.authService.renewSession()));
+
+    this.isMobile$.subscribe(result => {
+      this.isMobile = result;
+      this.isExpanded = result;
+    });
+  }
+
+  private isUserLoggedIn(){
+    var loggedIn = false;
+    this.isUserLoggedOut$.subscribe(loggedOut => {
+      if (!loggedOut) {
+        loggedIn = true;
+      }
+    });
+    return loggedIn;
+  }
+
+  toggleWhenMobile() {
+    if (this.isUserLoggedIn() && this.isMobile) {
+      this.isExpanded = true;
+      this.sidenav.toggle();
+    }
+  }
+
+  expandWhenDesktop() {
+    if (this.isUserLoggedIn() && !this.isMobile) {
+      this.isExpanded = !this.isExpanded;
+    }
+  }
+
+  closeWhenClicked(){
+    if (this.isUserLoggedIn() && this.isMobile) {
+      this.sidenav.close();
+    }
   }
 }
